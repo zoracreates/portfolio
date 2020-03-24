8711df29ebd0fdba99c04819947b9bbfab5da0b5
Zoraida <zmcabrera@gmail.com>
2020-03-23 23:03:18 -0400

    chore: update date messaging

--- src/components/posts-layout.js
+++ src/components/posts-layout.js
@@ -12,7 +12,7 @@ export default function PageTemplate({ data: { mdx } }) {
       <main className={`wrapper-lg`}>
       <h1>{mdx.frontmatter.title}</h1>
       {console.log(mdx.frontmatter.date)}
-      <p>{mdx.frontmatter.date && new Date(mdx.frontmatter.date).toLocaleDateString()}</p>
+      <p>Posted {mdx.frontmatter.date && new Date(mdx.frontmatter.date).toLocaleDateString()}</p>
       <MDXRenderer>{mdx.body}</MDXRenderer>
       </main>
     </Layout>
