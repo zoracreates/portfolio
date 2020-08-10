a1124c017838ba8ca484d7306621326bfee8a2ec
Zoraida <zmcabrera@gmail.com>
2020-08-10 15:38:19 -0400

    chore: new post

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = ({ data }) => {
             <h1>I'm Zoraida,<span> UX Designer/Researcher </span></h1>
             <p className={`hero-text wrapper-md`}>
               With a background in digital media design, web programming, and design research,
-              I create and improve digital products and experiences. <a href="https://hwp.harvard.edu/people/zoraida-cabrera-mieles" target="_blank">I work at Harvard University</a>, and
+              I create and improve digital products and experiences. <a href="https://hwp.harvard.edu/people/zoraida-cabrera-mieles" target="_blank" rel="noopener noreferrer">I work at Harvard University</a>, and
               I also love working with small businesses and creatives.
               On a more personal note, I’m learning to become a better advocate for ethical and inclusive design.
           </p>
