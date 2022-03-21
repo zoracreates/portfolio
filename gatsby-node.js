e750545469da6f472f56bd6da1ea074f577498a6
Zoraida <zmcabrera@gmail.com>
2022-03-21 18:20:07 -0400

    chore: update order of projects per imporance and not date

--- gatsby-node.js
+++ gatsby-node.js
@@ -92,7 +92,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const workResult = await graphql(`
     query WorkQuery {
       allMdx(
-        sort: { fields: [frontmatter___title], order: DESC }
+        sort: { fields: [frontmatter___importance], order: ASC }
         filter: { frontmatter: { published: { eq: true } } , fileAbsolutePath: {regex: "/posts/work/"} }
       ){
         edges {
